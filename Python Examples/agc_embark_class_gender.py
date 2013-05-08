c62990338b8c86e02a1835d9d96c9813335828ed
Andrew Conti <agc11d@gmail.com>
2013-05-08 16:41:46 -0400

    Updated Formating and Example.

--- Python Examples/agc_embark_class_gender.py
+++ Python Examples/agc_embark_class_gender.py
@@ -6,28 +6,43 @@
 # Here Will will run generate predictions of who survived and who did not
 # from our basic Least Squares Regression model.
 # Our Formula is :
-# survived_prediction = C + (b1 * female) + (b2 * U_Class) + (b3 * embarked_c))
+# survived_prediction = C + pclass + sex + age + sibsp  + embarked
 
 # Import Utilities
 import csv as csv
 import numpy as np
+import statsmodels.api as sm
+import kaggleaux as ka
+from pasty import dmatrices
+
 
 # declare necessary variables
-data = []   # a container to hold our test data in.
-x =  0      # a container for an iterator later on 
+data = []       # a container to hold our training data in.
+test_data = []  # a continer to hold our test data in. 
+x =  0          # a container for an iterator later on 
 
-# Regression Variables
-# independent variables, (what we are trying to predict)
-survived_prediction=0 # We initialize this variable as zero as a place holder
+with open('train.csv', 'rb') as f: # deals with opening and closing
+    csv_file = csv.reader(open('train.csv', 'rb'))
+    csv_file.next() # skips the header, so we can get to the data. 
+    for row in csv_file: 
+        data.append(row) 
 
-# dependent variables, (what we are using to make our predictions)
-# These are the coefficients we gained from training our OLS regression on 
-# the training data.
+# Then we convert  our list to NumPy array for more efficient data manipulation. 
+data = np.array(data) 
 
-C = float(0.077158358)
-U_ Class = float(0.234214501)
-female = float(0.512110314)
-embarked_c = float(0.111687285)
+# Model formula
+formula='survived ~ C(pclass) + C(sex) + age + sibsp  + C(embarked)'
+
+# Create a regression friendly version of our data using dmatrices
+y,x=dmatrices(formula, data=data, return_type='matrix')
+
+# Create a Logit Model Based on our data
+model=sm.Logit(y,x)
+
+# Fit that Model to the Data
+results= model.fit()
+
+results.params
 
 # The file is already trained on the train.csv file. 
 # Now we test our model by making predictions on the test.csv file. 
@@ -38,9 +53,10 @@ with open('test.csv', 'rb') as f:                  # Handel's the opening and cl
     csv_file = csv.reader(open('test.csv', 'rb'))
     csv_file.next()                                 # Skips the header in the test file, so we can get right to the data
     for row in csv_file: 
-        data.append(row) 
-    data = np.array(data)
+        test_data.append(row) 
+    test_data = np.array(data)
 
+predicted_results = ka.regress_pred_output(test_data, res, 'survived')
 # The print statements used throughout are designed to show what the program is doing
 # Once its executed in the terminal. 
 # The \n starts a new line. This allows for more attractive printing.  

8a647b6c1c3809c96f6d63dce9ba4fe8e86335e5
Andrew Conti <agc11d@gmail.com>
2013-05-08 12:09:34 -0400

    Improved Formatting in agc_embark_class_gender.py

--- Python Examples/agc_embark_class_gender.py
+++ Python Examples/agc_embark_class_gender.py
@@ -1,27 +1,27 @@
-# A model for prediction survival on the Titianic based on where an 
-# induvidual Embarked, thier gender, or the class they travled in. 
+# A model for prediction survival on the Titanic based on where an 
+# individual Embarked, their gender, or the class they traveled in. 
 # AGC 2013
 # 
 # 
 # Here Will will run generate predictions of who survived and who did not
 # from our basic Least Squares Regression model.
 # Our Formula is :
-# survived_prediction=(C+(b1female)+(b2U_Class)+(b3embarked_c))
+# survived_prediction = C + (b1 * female) + (b2 * U_Class) + (b3 * embarked_c))
 
-# Import utilites
+# Import Utilities
 import csv as csv
 import numpy as np
 
-# declare nessesary variables
-data = [] # a contianer to hold our test data in.
-x =  0
+# declare necessary variables
+data = []   # a container to hold our test data in.
+x =  0      # a container for an iterator later on 
 
 # Regression Variables
 # independent variables, (what we are trying to predict)
-survived_prediction=0 # We intialize this variale as zero as a place holder
+survived_prediction=0 # We initialize this variable as zero as a place holder
 
 # dependent variables, (what we are using to make our predictions)
-# These are the coefficents we gained from training our OLS regerssion on 
+# These are the coefficients we gained from training our OLS regression on 
 # the training data.
 
 C = float(0.077158358)
@@ -30,29 +30,30 @@ female = float(0.512110314)
 embarked_c = float(0.111687285)
 
 # The file is already trained on the train.csv file. 
-# Now we test our model by making predctions on the test.csv file. 
+# Now we test our model by making predictions on the test.csv file. 
 # You'll notice the test.csv file has no values in the survived field. This
-# Is what wer are tying to predict. 
+# Is what we're are tying to predict. 
 
-with open('test2.csv', 'rb') as f: # Handels the opening and closing of the file for us.
-    csv_file = csv.reader(open('test2.csv', 'rb'))
-    csv_file.next() # skips the header in the test file, so we can get right to the data
+with open('test.csv', 'rb') as f:                  # Handel's the opening and closing of the file for us.
+    csv_file = csv.reader(open('test.csv', 'rb'))
+    csv_file.next()                                 # Skips the header in the test file, so we can get right to the data
     for row in csv_file: 
         data.append(row) 
     data = np.array(data)
 
-# the print statements used throughout are desgined to show what the program is doing
-# once its executed in the terminal. 
+# The print statements used throughout are designed to show what the program is doing
+# Once its executed in the terminal. 
 # The \n starts a new line. This allows for more attractive printing.  
 print "\nBegin Predictions:" 
 
 
-# reads in the 'train' file for a comparative result
-cop_open_file = open("pred_embarkclassgendermodel.csv", "wb") # opens the file with write privilages ("wb")
-open_file = csv.writer(cop_open_file) # Theres no header in this guy
+# Reads in the 'train' file for a comparative result
+cop_open_file = open("results_embarkclassgendermodel.csv", "wb")    # Creates a csv file with write privileges ("wb") called
+                                                                    # results_embarkclassgendermodel.csv
+open_file = csv.writer(cop_open_file)                               # Theres no header in this guy
 
-with open('test2.csv', 'rb') as f: # Handels the opening and closing of the file for us.
-    csv_file = csv.reader(open('test2.csv', 'rb'))
+with open('test.csv', 'rb') as f:                                   # Handel's the opening and closing of the file for us.
+    csv_file = csv.reader(open('test.csv', 'rb'))
     csv_file.next()
     
     for row in csv_file:
@@ -65,8 +66,8 @@ with open('test2.csv', 'rb') as f: # Handels the opening and closing of the file
             pred_val = 1
         else:
             pred_val = 0
-        row.insert(0,pred_val) #Insert the prediciton at the start of the row
-        open_file.writerow(row) #Write the row to the file
+        row.insert(0,pred_val)  # Insert the prediction at the start of the row
+        open_file.writerow(row) # Write the row to the file
         x += 1
 
 cop_open_file.close()
